334916d2d8356a20135fddbeddb7588cebd45cd0
Thibaut <thibaut.leroy31@gmail.com>
2019-04-03 13:50:51 +0200

    ajout condition

--- hello.py
+++ hello.py
@@ -9,11 +9,14 @@ Operands and Operators, functions.
 Add two operands and replace compute method
 """
 resultat = 0 # Définition de la variable
-operande1 = -3
+operande1 = 3
 operande2 = 2
 resultat = operande1 + operande2
 print(resultat)
-
+if resultat > 0:
+    print("Résultat positif")
+else:
+    print("Résultat négatif")
 """
 Fin de l'algorithme
 """
