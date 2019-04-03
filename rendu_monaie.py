b45ba06bd01fa2fef3e28b4df53499160561b4b0
Thibaut <thibaut.leroy31@gmail.com>
2019-04-03 16:55:38 +0200

    rendu_monnaie4

--- rendu_monaie.py
+++ rendu_monaie.py
@@ -31,11 +31,31 @@ reste2 = piece2 % 2
 print("Vous avez acheté un barbecue pour un total de", achat, "€. Vous avez donné un total de", somme, "€. Nous vous devons", rendu, "€.")
 print("Je vous rend", billet100, "billets de 100€,", billet50, "billets de 50€,", billet20, "billets de 20€,", billet10, "billets de 10€,", billet5, "billets de 5€, et", piece2, "pièces de 2€. Merci de votre visite, et bonne journée.")
 
+listeBillet = [billet100,billet50,billet20,billet10,billet5,piece2]
+listeChaine = ["","","","","",""]
+listeMonnaie = [100,50,20,10,5,2]
+
+for i in range(6) :
+    if listeBillet[i] == 1:
+        listeChaine[i] = str(listeBillet[i]) + " billet de " + str(listeMonnaie[i]) + "€."
+    elif listeBillet[i] == 0:
+        listeChaine[i] = ""
+    else : 
+        listeChaine[i] = str(listeBillet[i]) + " billets de " + str(listeMonnaie[i]) + "€."
+
+print("Je vous rend :")
+for i in range(6):
+    if listeChaine[i] != "":
+        print(listeChaine[i])
+
+print("Merci !")
+
+
 print("La machine vous rend", billet100, "billets de 100€,", billet50, "billets de 50€,", billet20, "billets de 20€,", billet10, "billets de 10€,", billet5, "billets de 5€, et", piece2, "pièces de 2€. Merci de votre visite")
 
-if billet100 > 0:
-    print("La machine vous rend", billet100, "billets de 100€")
-else:
+if billet100 > 0: 
+    print("La machine vous rend", billet100, "billets de 100€") 
+else: 
     print()
 if billet50 > 0:
     print("La machine vous rend", billet50, "billets de 50€")
@@ -57,23 +77,3 @@ if piece2 > 0:
     print("La machine vous rend", piece2, "piece de 2€")
 else:
     print()
-
-
-listeBillet = [billet100,billet50,billet20,billet10,billet5,piece2]
-listeChaine = ["","","","","",""]
-listeMonnaie = [100,50,20,10,5,2]
-
-for i in range(6) :
-    if listeBillet[i] == 1:
-        listeChaine[i] = str(listeBillet[i]) + " billet de " + str(listeMonnaie[i]) + "€."
-    elif listeBillet[i] == 0:
-        listeChaine[i] = ""
-    else : 
-        listeChaine[i] = str(listeBillet[i]) + " billets de " + str(listeMonnaie[i]) + "€."
-
-print("Je vous rend :")
-for i in range(6):
-    if listeChaine[i] != "":
-        print(listeChaine[i])
-
-print("Merci !")
