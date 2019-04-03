18ae22453b990f30bae1c6c69383f245f9c83eee
Thibaut <thibaut.leroy31@gmail.com>
2019-04-03 16:31:45 +0200

    rendu_monnaie3

--- rendu_monaie.py
+++ rendu_monaie.py
@@ -12,10 +12,9 @@ Use input () function to get a user entry
 somme = input("Insérez vitre billet")
 """
 
-somme = 200
-achat1 = 3*10
-achat2 = 2*18
-rendu = somme - (achat1 + achat2)
+somme = 300
+achat = 219
+rendu = somme - achat
 billet100 = rendu // 100
 reste100 = rendu % 100
 billet50 = reste100 // 50
@@ -29,7 +28,8 @@ reste5 = reste10 % 5
 piece2 = reste5 // 2
 reste2 = piece2 % 2
 
-print("Vous avez acheté 3 croissants et 2 baguettes pour un total de", achat1 + achat2, "€. Vous avez donné un total de", somme, "€. La machine doit vous rendre", rendu, "€. Vous récupérez", billet100, "billets de 100€,", billet50, "billets de 50€,", billet20, "billets de 20€,", billet10, "billets de 10€,", billet5, "billets de 5€, et", piece2, "pièces de 2€. Merci de votre visite, et bonne journée.")
+print("Vous avez acheté un barbecue pour un total de", achat, "€. Vous avez donné un total de", somme, "€. Nous vous devons", rendu, "€.")
+print("Je vous rend", billet100, "billets de 100€,", billet50, "billets de 50€,", billet20, "billets de 20€,", billet10, "billets de 10€,", billet5, "billets de 5€, et", piece2, "pièces de 2€. Merci de votre visite, et bonne journée.")
 
 print("La machine vous rend", billet100, "billets de 100€,", billet50, "billets de 50€,", billet20, "billets de 20€,", billet10, "billets de 10€,", billet5, "billets de 5€, et", piece2, "pièces de 2€. Merci de votre visite")
 
