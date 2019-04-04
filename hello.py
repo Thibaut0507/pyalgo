764b949dbb923773633ce49443e2af11d673e1de
Thibaut <thibaut.leroy31@gmail.com>
2019-04-04 16:58:32 +0200

    manip_tableau4.py

--- hello.py
+++ hello.py
@@ -8,15 +8,31 @@ Operands and Operators, functions.
 @version 0.0.2
 Add two operands and replace compute method
 """
-resultat = 0 # Définition de la variable
-operande1 = 3
-operande2 = 2
-resultat = operande1 + operande2
+
+""" 
+Function Setting
+""" 
+def addition(operande1, operande2):
+    if operande1 < 0:
+        operande1 = operande1 * -1
+    if operande2 < 0:
+        operande2 = operande2 * -1
+    return operande1 + operande2
+
+resultat = addition(5, 3) # Call addition function with 5 and 3 as params
 print(resultat)
-if resultat > 0:
+print(addition(operande1, operande2))
+
+resultat = 0 # Définition de la variable
+operande1 = -3
+operande2 = 2
+resultat1 = operande1 + operande2
+print(resultat1)
+if resultat1 > 0:
     print("Résultat positif")
 else:
     print("Résultat négatif")
+
 """
 Fin de l'algorithme
 """
--- manip_tableau.py
+++ manip_tableau.py
@@ -5,9 +5,88 @@
     Algorithmique spécifique sur les collections
 """
 
+"""
+getLowerOf function
+return the lowest value of two params
+"""
+def getLowerOf(firstVal, secondVal):
+    if firstVal < secondVal:
+        return firstVal
+    else:
+        return secondVal
+
+"""
+getGreaterOf
+return the greater value of two params
+"""
+def getGreaterOf(firstVal, secondVal):
+    if firstVal > secondVal:
+        return firstVal
+    else: 
+        return secondVal
+
+"""
+compare function
+@param firstVal First Value to compare
+@param secondVal Second Value to compare
+@param howTo Mode de comparaison souhaité
+@return greater or lower value of two depends on desc params
+"""
+def compare(firstVal, secondVal, desc=True):
+    if (desc):
+        return getLowerOf(firstVal, secondVal)
+
+    return getGreaterOf(firstVal, secondVal)
+
+"""
+min function
+@param anArray The array from which i want to get the min value
+@return the min value of anArray
+"""
+def min(anArray):
+    theMin = anArray[0]
+    for value in anArray[1:]:
+        theMin = compare(theMin, value)
+
+    return theMin
+
+"""
+max function
+@param anArray The array from which i want to get the max value
+@return the max value of anArray
+"""
+def max(anArray):
+    theMax = anArray[0]
+    for value in anArray[1:]:
+        theMax = compare(theMax, value, False)
+
+    return theMax
+
+"""
+average function
+@param anArray Array from which evaluate the average value
+@return average value of an array
+"""
+def average(anArray):
+    total = 0
+    nbItems = 0
+    for val in anArray:
+        total = total + val
+        nbItems = nbItems +1
+    return total/nbItems
+
+"""
+Factorial function
+"""
+def factorial(anArray):
+    total = 1
+    for val in anArray:
+        total = total * val
+    return total
+
 #Déclaration du tableau de démonstration
 
-monTablo = [15, 3, 25, 12, 7, -15, 45, -25, 71, 14, -78, 21, 1]
+monTablo = [15, 3, 25, 12, 7, -15]
 
 #
 # Simple poor loop
@@ -15,7 +94,7 @@ monTablo = [15, 3, 25, 12, 7, -15, 45, -25, 71, 14, -78, 21, 1]
 for indice, val in enumerate(monTablo):
     print(monTablo[indice]*2)
 
-print("*****************")
+print("*********************")
 
 # More complex loop with condition
 for indice, val in enumerate(monTablo):
@@ -29,7 +108,7 @@ for val in monTablo:
         print(monTablo[indice] * 2)
     indice = indice + 1
 
-print("*****************")
+print("*********************")
 
 # Trouver la valeur la plus petite du tableau
 initiale = monTablo[0]
@@ -50,21 +129,58 @@ mayBeImTheMin = monTablo[0] # Just initialize the min as the first item in the a
 for val in monTablo[1:]: # Loop over the array from second element
     if val < mayBeImTheMin: # Check if the current value in the array is lower than the current
         mayBeImTheMin = val
-# Fianlly... 
+# Finally... 
 print("And the min is : ", mayBeImTheMin)
+
+# Bit more complex loop : find a min with function
+mayBeImTheMin = monTablo[0] # Just initialize the min as the first item in the array
+for val in monTablo[1:]: # Loop over the array from second element
+    mayBeImTheMin = getLowerOf(mayBeImTheMin, val)
+# Finally... 
+print("And the min is : ", mayBeImTheMin)
+
+# Bit more complex loop : find a min with function compare
+mayBeImTheMin = monTablo[0] # Just initialize the min as the first item in the array
+for val in monTablo[1:]: # Loop over the array from second element
+    mayBeImTheMin = compare(mayBeImTheMin, val)
+# Finally... 
+print("And the min is : ", mayBeImTheMin)
+
+print("And the min is : ", min(monTablo))
+
+
+print("*********************")
+
 
 # Bit more complex loop : find a max
 mayBeImTheMax = monTablo[0] # Just initialize the max as the first item in the array
-for val in monTablo[1:]: # Loop over the array from second element
+for val in monTablo[1:]: # Loop over the array from second element --> inutile de passer par le premier element vu qu'on le prend comme valeur de départ
     if val > mayBeImTheMax: # Check if the current value in the array is greater than the current
         mayBeImTheMax = val
-# Fianlly... 
+# Finally... 
 print("And the max is : ", mayBeImTheMax)
 
-# Déterminer valeur min (fonction min)
-Minimal = min(monTablo)
-print("La valeur minimale est : ", Minimal)
+# Bit more complex loop : find a max with function
+mayBeImTheMax = monTablo[0] # Just initialize the min as the first item in the array
+for val in monTablo[1:]: # Loop over the array from second element
+    mayBeImTheMax = getGreaterOf(mayBeImTheMax, val)
+# Finally... 
+print("And the max is : ", mayBeImTheMax)
 
-# Déterminer valeur max (fonction max)
-Maximal = max(monTablo)
-print("La valeur maximale est : ", Maximal)
+# Bit more complex loop : find a max with function compare
+mayBeImTheMax = monTablo[0] # Just initialize the min as the first item in the array
+for val in monTablo[1:]: # Loop over the array from second element
+    mayBeImTheMax = compare(mayBeImTheMax, val, False)
+# Finally... 
+print("And the max is : ", mayBeImTheMax)
+
+print("And the max is : ", max(monTablo))
+
+
+print("*********************")
+
+print("La moyenne du tableau est : ", average(monTablo))
+
+print("*********************")
+
+print("La factorielle du tableau est : ", factorial(monTablo))
